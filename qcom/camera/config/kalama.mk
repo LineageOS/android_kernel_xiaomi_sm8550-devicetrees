a2c0b3013626e00bcce43b786612c96d7842be46
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-04-11 17:29:42 +0200

    camera: Import Xiaomi devicetree changes
     * from sheng-u-oss
    
    Change-Id: Ia121fd095acc9278b99f2e83a8ff6af06073ad23
    (cherry picked from commit 80516b3386eea2823ca8b90b9d5f142442f13c52)

--- qcom/camera/config/kalama.mk
+++ qcom/camera/config/kalama.mk
@@ -4,7 +4,8 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
 								kalama-camera-sensor-hdk.dtbo \
 								kalama-sg-hhg-camera.dtbo \
-								kalama-sg-hhg-camera-sensor.dtbo
+								kalama-sg-hhg-camera-sensor.dtbo \
+								socrates-sm8550-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
 				crow-camera-sensor-idp.dtbo \
