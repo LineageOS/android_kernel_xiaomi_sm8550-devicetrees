a458c9e6f78bc975a883a2779e347007d1303461
Bruno Martins <bgcngm@gmail.com>
2024-06-26 21:31:36 +0200

    treewide: Opt-out for irrelevant DTB[O]s based on config
    Change-Id: Ic2bcdb5533f21005708868e4168391563c9961c7

--- qcom/camera/config/kalama.mk
+++ qcom/camera/config/kalama.mk
@@ -1,11 +1,15 @@
 dtbo-$(CONFIG_ARCH_KALAMA) := kalama-camera.dtbo
+
+ifeq ($(CONFIG_TARGET_PRODUCT_SOCRATES),y)
+dtbo-y += socrates-sm8550-camera-sensor.dtbo
+else
 dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-mtp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
 								kalama-camera-sensor-hdk.dtbo \
 								kalama-sg-hhg-camera.dtbo \
-								kalama-sg-hhg-camera-sensor.dtbo \
-								socrates-sm8550-camera-sensor.dtbo
+								kalama-sg-hhg-camera-sensor.dtbo
+endif
 
 dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
 				crow-camera-sensor-idp.dtbo \
